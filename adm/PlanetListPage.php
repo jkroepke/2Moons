0aaeea6c1839048979091accdb487c2a2604d890
Jan Kröpke <kroepke@outlook.com>
2010-03-31 19:55:29 +0000

    Fix KB - FIX PlanetListPage.php - Fix Insaller

--- adm/PlanetListPage.php
+++ adm/PlanetListPage.php
@@ -23,10 +23,10 @@ define('INSIDE'  , true);
 define('INSTALL' , false);
 define('IN_ADMIN', true);
 
-define('ROOT_PATH', './../');
-include(ROOT_PATH . 'extension.inc');
-include(ROOT_PATH . 'common.' . PHP_EXT);
-include('AdminFunctions/Autorization.' . PHP_EXT);
+$xgp_root = './../';
+include($xgp_root . 'extension.inc.php');
+include($xgp_root . 'common.' . $phpEx);
+include('AdminFunctions/Autorization.' . $phpEx);
 
 if ($EditUsers != 1) die();
 
@@ -52,76 +52,6 @@ if ($EditUsers != 1) die();
 	else
 	$parse['planetes'] .= "<tr><th class=b colspan=8>Insgesamt {$i} Planeten</th></tr>";
 
-	if(isset($_GET['action']) && isset($_GET['id'])) {
-		$id 	= intval($_GET['id']);
-		$query  = $db->query("SELECT * FROM ".PLANETS." WHERE planet_type=1 AND id='".$id."' LIMIT 1");
-		$parse 	= array_merge($db->fetch_array($query), $lang);
-		
-		$parse['show_edit_form'] = parsetemplate(gettemplate('adm/PlanetListEdit'),$parse);
-	}
-	if(isset($_POST['submit'])) {
-
-		$edit_id 	= intval($_POST['currid']);
-		$planetname = $db->sql_escape($_POST['planetname']);
-		$fields_max = intval($_POST['felder']);
-		$db->query("UPDATE ".PLANETS." SET
-							`name` 				= '".$planetname."', 
-							`field_max` 		= '".$fields_max."',
-							`metal`				= '".$_POST['metal']."',
-							`crystal`			= '".$_POST['crystal']."',
-							`deuterium`			= '".$_POST['deuterium']."', 
-							`small_ship_cargo` 	= '".$_POST['small_ship_cargo']."', 
-							`big_ship_cargo` 	= '".$_POST['big_ship_cargo']."', 
-							`light_hunter`		= '".$_POST['light_hunter']."', 
-							`heavy_hunter`		= '".$_POST['heavy_hunter']."', 
-							`crusher`			= '".$_POST['crusher']."', 
-							`battle_ship`		= '".$_POST['battle_ship']."', 
-							`colonizer`			= '".$_POST['colonizer']."', 
-							`recycler`			= '".$_POST['recycler']."', 
-							`spy_sonde`			= '".$_POST['spy_sonde']."', 
-							`bomber_ship`		= '".$_POST['bomber_ship']."', 
-							`solar_satelit`		= '".$_POST['solar_satelit']."', 
-							`destructor`		= '".$_POST['destructor']."', 
-							`dearth_star`		= '".$_POST['dearth_star']."', 
-							`lune_noir`			= '".$_POST['lune_noir']."',
-							`ev_transporter`	= '".$_POST['ev_transporter']."',
-							`star_crasher`		= '".$_POST['star_crasher']."',
-							`giga_recykler`		= '".$_POST['giga_recykler']."',
-							`dm_ship`			= '".$_POST['dm_ship']."',
-							`misil_launcher`	= '".$_POST['misil_launcher']."',
-							`small_laser`		= '".$_POST['small_laser']."',
-							`big_laser`			= '".$_POST['big_laser']."',
-							`gauss_canyon`		= '".$_POST['gauss_canyon']."',
-							`ionic_canyon`		= '".$_POST['ionic_canyon']."',
-							`buster_canyon`		= '".$_POST['buster_canyon']."',
-							`small_protection_shield` 	= '".$_POST['small_protection_shield']."',
-							`big_protection_shield` 	= '".$_POST['big_protection_shield']."',
-							`interceptor_misil` 		= '".$_POST['interceptor_misil']."',
-							`interplanetary_misil` 		= '".$_POST['interplanetary_misil']."',
-							`metal_mine`				= '".$_POST['metal_mine']."',
-							`crystal_mine`				= '".$_POST['crystal_mine']."',
-							`deuterium_sintetizer`		= '".$_POST['deuterium_sintetizer']."',
-							`solar_plant`				= '".$_POST['solar_plant']."',
-							`fusion_plant`				= '".$_POST['fusion_plant']."',
-							`robot_factory`				= '".$_POST['robot_factory']."',
-							`nano_factory`				= '".$_POST['nano_factory']."',
-							`hangar`					= '".$_POST['hangar']."',
-							`metal_store`				= '".$_POST['metal_store']."',
-							`crystal_store`				= '".$_POST['crystal_store']."',
-							`deuterium_store`			= '".$_POST['deuterium_store']."',
-							`laboratory`				= '".$_POST['laboratory']."',
-							`terraformer`				= '".$_POST['terraformer']."',
-							`ally_deposit`				= '".$_POST['ally_deposit']."',
-							`silo`						= '".$_POST['silo']."'
-							  WHERE `id` = '".$edit_id."';");
-		// AdminLOG - Helmchen
-		$fp = @fopen('logs/adminlog_'.date('d.m.Y').'.txt','a');
-		fwrite($fp,date("d.m.Y H:i:s",time())." - ".$user['username']." - ".$user['user_lastip']." - ".__FILE__." - changed values from planet with ID: ".$edit_id."\n");
-		fclose($fp);
-		// AdminLOG ENDE
-
-		header("location:PlanetListPage.php");
-	}
 	display(parsetemplate(gettemplate('adm/PlanetListBody'), $parse), false, '', true, false);
 
 // Created by e-Zobar. All rights reversed (C) XNova Team 2008
