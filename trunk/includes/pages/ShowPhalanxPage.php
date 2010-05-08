6179d77628cf3bbde57cee064da25eb63bf2c55d
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-05-08 02:30:29 +0000

    - FIX Phalax

--- trunk/includes/pages/ShowPhalanxPage.php
+++ trunk/includes/pages/ShowPhalanxPage.php
@@ -47,7 +47,7 @@ function ShowPhalanxPage($CurrentUser, $CurrentPlanet)
 	$DoScan 			= ($System <= $SystemLimitMax && $System >= $SystemLimitMin && $Galaxy == $CurrentPlanet['galaxy']) ? true : false;
 
 	if ($CurrentPlanet['deuterium'] > 5000)
-		$db->query("UPDATE ".PLANETS." SET `deuterium` = `deuterium` - '5000' WHERE `id` = '". $CurrentPlanet['id'] ."';");
+		$CurrentPlanet['deuterium'] -= 5000;
 	else
 	{
 		$template->message($lang['px_no_deuterium']);
