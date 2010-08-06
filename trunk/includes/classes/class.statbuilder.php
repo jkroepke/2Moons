46a1293cdf02f1283364dfca73f93f68f09035a2
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-08-06 10:47:53 +0000

    fixed Date on topnav for Account Deletion

--- trunk/includes/classes/class.statbuilder.php
+++ trunk/includes/classes/class.statbuilder.php
@@ -48,7 +48,7 @@ class statbuilder{
 		
 		$db->multi_query("DELETE FROM `".MESSAGES."` WHERE `message_time` < '". $del_before ."';DELETE FROM ".SUPP." WHERE `time` < '".$del_before."' AND `status` = 0;DELETE FROM ".CHAT." WHERE `timestamp` < '".$del_before."';DELETE FROM ".ALLIANCE." WHERE `ally_members` = '0';DELETE FROM ".PLANETS." WHERE `destruyed` < ".TIMESTAMP." AND `destruyed` != 0;UPDATE ".USERS." SET `email_2` = `email` WHERE `setmail` < '".TIMESTAMP."';");
 
-		$ChooseToDelete = $db->query("SELECT `id` FROM `".USERS."` WHERE ((`db_deaktjava` < '".$del_deleted."' AND `db_deaktjava` <> 0) OR `onlinetime` < '".$del_inactive."') AND `authlevel` = '0';");
+		$ChooseToDelete = exit("SELECT `id` FROM `".USERS."` WHERE `authlevel` = '0' AND ((`db_deaktjava` != 0 AND `db_deaktjava` < '".$del_deleted."') OR `onlinetime` < '".$del_inactive."');");
 		
 		if(isset($ChooseToDelete))
 		{
