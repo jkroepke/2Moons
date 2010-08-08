f571256198b8e36c6e492258b80d46d71ced9e52
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-08-08 15:55:24 +0000

    Fixed Teamspeak Settings.

--- trunk/includes/pages/adm/ShowTeamspeakPage.php
+++ trunk/includes/pages/adm/ShowTeamspeakPage.php
@@ -27,23 +27,23 @@ function ShowTeamspeakPage() {
 	if ($_POST)
 	{
 		if (isset($_POST['ts_on']) && $_POST['ts_on'] == 'on') {
-			$CONF['ts_on'] = 1;
+			$CONF['ts_modon'] = 1;
 		} else {
-			$CONF['ts_on'] = 0;
+			$CONF['ts_modon'] = 0;
 		}
 		
-		$CONF['ts_ip']	= request_var('ts_ip', '');
-		$CONF['ts_tcp']	= request_var('ts_tcp', 0);
-		$CONF['ts_udp']	= request_var('ts_udp', 0);
-		$CONF['ts_to']	= request_var('ts_to', 0);
-		$CONF['ts_v']	= request_var('ts_v', 0);
+		$CONF['ts_server']	= request_var('ts_ip', '');
+		$CONF['ts_tcpport']	= request_var('ts_tcp', 0);
+		$CONF['ts_udpport']	= request_var('ts_udp', 0);
+		$CONF['ts_timeout']	= request_var('ts_to', 0);
+		$CONF['ts_version']	= request_var('ts_v', 0);
 		
-		update_config('ts_timeout'	, $CONF['ts_to']);
-		update_config('ts_modon'	, $CONF['ts_on']);
-		update_config('ts_server'	, $CONF['ts_ip']);
-		update_config('ts_tcpport'	, $CONF['ts_tcp']);
-		update_config('ts_udpport'	, $CONF['ts_udp']);
-		update_config('ts_version'	, $CONF['ts_v']);
+		update_config('ts_timeout'	, $CONF['ts_timeout']);
+		update_config('ts_modon'	, $CONF['ts_modon']);
+		update_config('ts_server'	, $CONF['ts_server']);
+		update_config('ts_tcpport'	, $CONF['ts_tcpport']);
+		update_config('ts_udpport'	, $CONF['ts_udpport']);
+		update_config('ts_version'	, $CONF['ts_version']);
 	}	$template	= new template();
 	
 	$template->page_header();
@@ -58,12 +58,12 @@ function ShowTeamspeakPage() {
 		'ts_timeout'			=> $LNG['ts_timeout'],
 		'ts_server_query'		=> $LNG['ts_server_query'],
 		'ts_server_id'			=> $LNG['ts_server_id'],
-		'ts_to'					=> $CONF['ts_to'],
-		'ts_on'					=> $CONF['ts_on'],
-		'ts_ip'					=> $CONF['ts_ip'],
-		'ts_tcp'				=> $CONF['ts_tcp'],
-		'ts_udp'				=> $CONF['ts_udp'],
-		'ts_v'					=> $CONF['ts_v'],
+		'ts_to'					=> $CONF['ts_timeout'],
+		'ts_on'					=> $CONF['ts_modon'],
+		'ts_ip'					=> $CONF['ts_server'],
+		'ts_tcp'				=> $CONF['ts_tcpport'],
+		'ts_udp'				=> $CONF['ts_udpport'],
+		'ts_v'					=> $CONF['ts_version'],
 	));
 	$template->show('adm/TeamspeakPage.tpl');
 
