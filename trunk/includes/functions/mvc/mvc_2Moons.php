9f6e12a096803829bd3ae88cd7d78ea56311d00d
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-08-15 12:30:11 +0000

    Update to Rev 446 | Part 3

---
+++ trunk/includes/functions/mvc/mvc_2Moons.php
@@ -0,0 +1,13 @@
+<?php
+
+$MVC[]	= array(
+	'author'		=> 'Slaver',
+	'title'			=> '2Moons',
+	'tag'			=> 'moons_update',
+	'version'		=> '1.0',
+	'link'			=> 'http://www.xnova-reloaded.eu/index.php?page=Thread&threadID=3126',
+	'update'		=> 'http://www.2moons-systems.com/update.xml',
+	'description'	=> array('de' => 'Basis', 'en' => 'Basic', 'es' => 'Básico', 'ru' => 'Основной', 'pt' => 'Básico', 'cn' => '基本的', 'hr' => 'Osnovni'),
+);
+
+?>
