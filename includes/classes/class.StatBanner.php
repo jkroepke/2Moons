66149947eb50d4629ed62e7c0eecca6ef4438a3a
Jan Kröpke <kroepke@outlook.com>
2010-08-19 05:08:07 +0000

    Fixed Forschungspage.

--- includes/classes/class.StatBanner.php
+++ includes/classes/class.StatBanner.php
@@ -45,49 +45,51 @@ class StatBanner {
 
 		// Parameters
 
-		$image  = imagecreatefrompng($this->source);
+		$image  = imagecreatefrompng(ROOT_PATH.$this->source);
 		$date   = date("d.m.y");
 		
 		// Querys
-		$Query = $db->fetch_array($db->query("SELECT a.username, b.build_points, b.fleet_points, b.defs_points, b.tech_points, b.total_points, b.total_rank, c.name, c.galaxy, c.system, c.planet FROM ".USERS." as a, ".STATPOINTS." as b, ".PLANETS." as c WHERE a.id = '".$id."' AND b.stat_type = '1' AND b.stat_code = '1' AND b.id_owner = '".$id."' AND c.id = a.id_planet;"));
+		$Query = $db->uniquequery("SELECT a.username, b.build_points, b.fleet_points, b.defs_points, b.tech_points, b.total_points, b.total_rank, c.name, c.galaxy, c.system, c.planet FROM ".USERS." as a, ".STATPOINTS." as b, ".PLANETS." as c WHERE a.id = '".$id."' AND b.stat_type = '1' AND b.stat_code = '1' AND b.id_owner = '".$id."' AND c.id = a.id_planet;");
 		// Variables
 		$b_univ   = $CONF['game_name'];
-		$b_user   = utf8_decode($Query['username']);
-		$b_planet = utf8_decode($Query['name']);
+		$b_user   = $Query['username'];
+		$b_planet = $Query['name'];
 		$b_xyz    = "[".$Query['galaxy'].":".$Query['system'].":".$Query['planet']."]";
-		$b_lvl    = "".$Query['total_rank']  ."/".$CONF['users_amount']."";
-		$b_build  = "".utf8_decode($LNG['st_buildings']) .": ".pretty_number($Query['build_points'])."";
-		$b_fleet  = "".utf8_decode($LNG['st_fleets']) .": ".pretty_number($Query['fleet_points'])."";
-		$b_def    = "".utf8_decode($LNG['st_defenses']) .": ".pretty_number($Query['defs_points'])."";
-		$b_search = "".utf8_decode($LNG['st_researh']) .": ".pretty_number($Query['tech_points'])."";
-		$b_total  = "".utf8_decode($LNG['st_points']) .": ".pretty_number($Query['total_points'])."";
+		$b_lvl    = $Query['total_rank']  ."/".$CONF['users_amount']."";
+		$b_build  = utf8_encode($LNG['st_buildings'] .": ".pretty_number($Query['build_points']));
+		$b_fleet  = $LNG['st_fleets'] .": ".pretty_number($Query['fleet_points'])."";
+		$b_def    = $LNG['st_defenses'] .": ".pretty_number($Query['defs_points'])."";
+		$b_search = $LNG['st_researh'] .": ".pretty_number($Query['tech_points'])."";
+		$b_total  = $LNG['st_points'] .": ".pretty_number($Query['total_points'])."";
 
 		// Colors
 		$red    = hexdec(substr($this->textcolor,0,2));
 		$green  = hexdec(substr($this->textcolor,2,4));
 		$blue   = hexdec(substr($this->textcolor,4,6));
-		$select = imagecolorallocate($image,$red,$green,$blue);
+		$color  = imagecolorallocate($image,$red,$green,$blue);
 
+		$font 	= ROOT_PATH.'scripts/arial.ttf';
+		
 		// Display
 		// Univers name
-		imagestring($image, 1, $this->CenterTextBanner($b_univ,1,653), 57, $b_univ, $select);
+		imagettftext($image, 12, 0, $this->CenterTextBanner($b_univ,1,653), 57, $color, $font, $b_univ);
 		// Today date
-		imagestring($image, 1, $this->CenterTextBanner($date,1,653), 65, $date, $select);
+		imagettftext($image, 12, 0, $this->CenterTextBanner($date,1,653), 65, $color, $font, $date);
 		// Player name
-		imagestring($image, 3, 15, 12, $b_user, $select);
+		imagettftext($image, 14, 0, 15, 12 ,$color, $font, $b_user);
 		// Player b_planet
-		imagestring($image, 3, 150, 12, $b_planet." ".$b_xyz, $select);
+		imagettftext($image, 14, 0, 150, 12 ,$color, $font, $b_planet." ".$b_xyz);
 		// Player level
-		imagestring($image, 10, $this->CenterTextBanner($b_lvl,10,795), 40, $b_lvl, $select);
+		imagettftext($image, 12, 0, $this->CenterTextBanner($b_lvl,10,795), 40 ,$color, $font, $b_lvl);
 		// Player stats
-		imagestring($image, 2, 15,  30, $b_build,  $select);
-		imagestring($image, 2, 15,  45, $b_fleet,  $select);
-		imagestring($image, 2, 170, 30, $b_search, $select);
-		imagestring($image, 2, 170, 45, $b_def,  $select);
-		imagestring($image, 2, 15,  60, $b_total,  $select);
+		imagettftext($image, 12, 0, 15, 30, $color, $font, $b_build);
+		imagettftext($image, 12, 0, 15, 45, $color, $font, $b_fleet);
+		imagettftext($image, 12, 0, 170, 30, $color, $font, $b_search);
+		imagettftext($image, 12, 0, 170, 45, $color, $font, $b_def);
+		imagettftext($image, 12, 0, 15, 60, $color, $font, $b_total);
 
 		// Creat and delete banner
-		ImagePNG($image,$this->path.$id.".png");
+		ImagePNG($image,ROOT_PATH.$this->path.$id.".png");
 		imagedestroy($image);
 	}
 
@@ -150,7 +152,7 @@ class StatBanner {
 	public function ShowStatBanner($id) 
 	{
 
-		if(!file_exists($this->path.$id.".png")){
+		if(file_exists($this->path.$id.".png")){
 			$this->BuildIMGforID($id);
 		}
 
