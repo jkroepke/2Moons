45bf23d92dc713c0de0ad1fb02831ec04041a07c
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-09-24 21:12:41 +0000

    Fixed Fleet1 Error

--- trunk/scripts/overview.js
+++ trunk/scripts/overview.js
@@ -60,3 +60,14 @@ function BuildTime() {
 	$('#blc').text(GetRestTimeFormat(s));
 	window.setTimeout('BuildTime()', 1000);
 }
+
+function FleetTime() {
+	$.each(Fleets, function(id, time) {
+		var s		= (time - (serverTime.getTime() / 1000) + ServerTimezoneOffset);
+		if(s <= 0) {
+			$('#fleettime_'+id).text('-');
+		} else {
+			$('#fleettime_'+id).text(GetRestTimeFormat(s));
+		}
+	});
+}
