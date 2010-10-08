079e95876ea195b140fb36ea1a0d6749b71c2d44
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-10-08 20:17:20 +0000

    Added ro.stelian.sorin Line Commit

--- trunk/includes/classes/missions/MissionCaseExpedition.php
+++ trunk/includes/classes/missions/MissionCaseExpedition.php
@@ -150,7 +150,7 @@ class MissionCaseExpedition extends MissionFunctions
 				$MinFound		= mt_rand(7000, 10000);
 			
 				$FoundShipMess	= "";	
-				$NewFleetArray 	= "";
+				$NewFleetArray 	= $this->_fleet['fleet_array'];
 
 				$LNG			+= $this->GetUserLang($this->_fleet['fleet_owner'], 'TECH');
 				
