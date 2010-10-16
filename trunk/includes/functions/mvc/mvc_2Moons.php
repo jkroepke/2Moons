dea2ad46d675a8909eea70308a0af54d008eb5d1
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-10-16 08:37:22 +0000

    Updated MVC

--- trunk/includes/functions/mvc/mvc_2Moons.php
+++ trunk/includes/functions/mvc/mvc_2Moons.php
@@ -4,7 +4,7 @@ $MVC[]	= array(
 	'author'		=> 'Slaver',
 	'title'			=> '2Moons',
 	'tag'			=> 'moons_update',
-	'version'		=> '1.0',
+	'version'		=> '1.2',
 	'link'			=> 'http://www.xnova-reloaded.eu/index.php?page=Thread&threadID=3126',
 	'update'		=> 'http://update.2moons-systems.com/update.xml',
 	'description'	=> array('de' => 'Basis', 'en' => 'Basic', 'es' => 'Básico', 'ru' => 'Основной', 'pt' => 'Básico', 'cn' => '基本的', 'hr' => 'Osnovni'),
