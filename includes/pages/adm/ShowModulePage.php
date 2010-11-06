49c0405ad1b7651d386020f1d07b7e645fee9666
Jan Kröpke <kroepke@outlook.com>
2010-11-06 20:58:15 +0000

    Fixed FB Login Fixed Moduls Page

--- includes/pages/adm/ShowModulePage.php
+++ includes/pages/adm/ShowModulePage.php
@@ -27,7 +27,7 @@ function ShowModulePage()
 	global $CONF, $LNG;
 	if($_GET['mode']) {
 		$CONF['moduls'][request_var('id', 0)]	= ($_GET['mode'] == 'aktiv') ? 1 : 0;
-		update_config(array('moduls' => implode(";", $CONF['moduls']), false, $_SESSION['adminuni']);
+		update_config(array('moduls' => implode(";", $CONF['moduls']), false, $_SESSION['adminuni']));
 		$CONF['moduls']		= explode(";", $CONF['moduls']);
 	}
 
