a3105c5f8ff3fd9fff640e596bd466e731921cdc
robbyn666 <robbyn666@b16e238e-0886-11df-aae1-e754eacfc418>
2010-11-15 16:14:05 +0000

    ADD Backupsystem in ACP

--- trunk/includes/pages/adm/ShowMenuPage.php
+++ trunk/includes/pages/adm/ShowMenuPage.php
@@ -62,6 +62,7 @@ function ShowMenuPage()
 		'mu_search_page'			=> $LNG['mu_search_page'],
 		'mu_mod_update'				=> $LNG['mu_mod_update'],
 		'mu_clear_cache'			=> $LNG['mu_clear_cache'],
+		'mu_backup_page'			=> $LNG['mu_backup_page'],
 	));
 	
 	$template->show('adm/ShowMenuPage.tpl');
