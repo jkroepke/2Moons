e3e8acd4dcb58a866d497ae004d5676b1d277093
Lyon731 <Lyon731@b16e238e-0886-11df-aae1-e754eacfc418>
2010-11-24 16:52:20 +0000

    FIX-Usersuche und Allyanzeige

--- trunk/includes/pages/ShowSearchPage.php
+++ trunk/includes/pages/ShowSearchPage.php
@@ -79,7 +79,7 @@ function ShowSearchPage()
 			$db->free_result($search);
 		break;
 		case "allytag":
-echo "SELECT a.id, a.username, a.ally_id, a.ally_name, a.galaxy, a.system, a.planet, b.name, c.total_rank FROM ".USERS." as a LEFT JOIN ".PLANETS." as b ON b.id = a.id_planet LEFT JOIN ".STATPOINTS." as c ON c.stat_type = 1 AND c.id_owner = a.id WHERE a.`universe` = '".$UNI."' AND a.username LIKE '%".$db->sql_escape($searchtext, true)."%' LIMIT 25;";
+		$search = $db->query("SELECT a.id, a.username, a.ally_id, a.ally_name, a.galaxy, a.system, a.planet, b.name, c.total_rank FROM ".USERS." as a LEFT JOIN ".PLANETS." as b ON b.id = a.id_planet LEFT JOIN ".STATPOINTS." as c ON c.stat_type = 1 AND c.id_owner = a.id WHERE a.`universe` = '".$UNI."' AND a.username LIKE '%".$db->sql_escape($searchtext, true)."%' LIMIT 25;";)
 			while($s = $db->fetch_array($search))
 			{
 				$SearchResult[]	= array(
