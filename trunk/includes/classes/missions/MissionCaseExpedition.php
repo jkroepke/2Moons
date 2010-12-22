1838019ca7083130d5d35ffef5fc4820110aa206
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-12-22 06:15:57 +0000

    Fixed PHP Error On Expedition

--- trunk/includes/classes/missions/MissionCaseExpedition.php
+++ trunk/includes/classes/missions/MissionCaseExpedition.php
@@ -147,7 +147,7 @@ class MissionCaseExpedition extends MissionFunctions
 				$FoundShipMess	= "";	
 				$NewFleetArray 	= "";
 
-				$LNG			+= $this->GetUserLang($this->_fleet['fleet_owner'], 'TECH');
+				$LNG			+= $LANG->GetUserLang($this->_fleet['fleet_owner'], 'TECH');
 				$Found			= array();
 				foreach($reslist['fleet'] as $ID) 
 				{
