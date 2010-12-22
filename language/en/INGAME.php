c0d9d24dc7ab02746f2bc127890a5bb3d4783243
Jan Kröpke <kroepke@outlook.com>
2010-12-22 15:03:36 +0000

    Updated Language

--- language/en/INGAME.php
+++ language/en/INGAME.php
@@ -1,13 +1,5 @@
 <?php
 
-//LANUAGES
-$LNG['langs']    = array(
-    'de' => 'Deutsch', 
-    'en' => 'English', 
-    'ru' => 'Русский', 
-    'pt' => 'Português',
-    'cn' => '简体中文',
-);
 //SERVER GENERALS
 $LNG['Metal']								= 'Metal';
 $LNG['Crystal']								= 'Crystal';
