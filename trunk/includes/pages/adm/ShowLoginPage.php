6492c443e0550396ffda77a7186ef0ed1d7c179c
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2010-12-27 18:59:24 +0000

    Fixed UMode Build Bug.

--- trunk/includes/pages/adm/ShowLoginPage.php
+++ trunk/includes/pages/adm/ShowLoginPage.php
@@ -20,7 +20,7 @@
 ##############################################################################
 function ShowLoginPage()
 {
-	global $USER;
+	global $USER, $LNG;
 	if(isset($_REQUEST['admin_pw']) && md5($_REQUEST['admin_pw']) == $USER['password']) {
 		$_SESSION['admin_login']	= md5($_REQUEST['admin_pw']);
 		redirectTo('admin.php');
@@ -29,7 +29,9 @@ function ShowLoginPage()
 	$template	= new template();
 	$template->page_header();
 	$template->assign_vars(array(	
-		
+		'adm_login'			=> $LNG['adm_login'],
+		'adm_password'			=> $LNG['adm_password'],
+		'adm_absenden'			=> $LNG['adm_absenden'],
 	));
 	$template->show('adm/LoginPage.tpl');
 }
--- trunk/styles/templates/adm/LoginPage.tpl
+++ trunk/styles/templates/adm/LoginPage.tpl
@@ -3,10 +3,10 @@
 	<form action="" method="POST">
     <table style="width:569px;margin-top:30px;">
 		<tr>
-            <td class="c">Login</td>
+            <td class="c">{$adm_login}</td>
         </tr>
 		<tr>
-            <th>Password: <input type="password" name="admin_pw"><br><input type="submit" value="Absenden"></th></th>
+            <th>{$adm_password}: <input type="password" name="admin_pw"><br><input type="submit" value="{$adm_absenden}"></th></th>
         </tr>
     </table>
 	</form>
