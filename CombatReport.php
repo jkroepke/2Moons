0ddb6f1afee273ee0f845c2c19b6416427273123
robbyn666 <robbyn666@b16e238e-0886-11df-aae1-e754eacfc418>
2011-01-01 21:38:38 +0000

    - FIX CombatReport error

--- CombatReport.php
+++ CombatReport.php
@@ -25,6 +25,7 @@ define('INSTALL' , false);
 define('ROOT_PATH', './');
 include(ROOT_PATH . 'extension.inc');
 include(ROOT_PATH . 'common.'.PHP_EXT);
+require_once(ROOT_PATH . 'includes/functions/SortUserPlanets.'.PHP_EXT);
 
 $LANG->includeLang(array('FLEET', 'TECH'));
 	
