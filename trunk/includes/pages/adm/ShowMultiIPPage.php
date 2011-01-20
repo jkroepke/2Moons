8348e728f39e2626bf3efbd89c6f57617dd4b1d2
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-01-20 09:04:40 +0000

    Removed CrossUniversse Infofations on MultiIPPage

--- trunk/includes/pages/adm/ShowMultiIPPage.php
+++ trunk/includes/pages/adm/ShowMultiIPPage.php
@@ -25,7 +25,7 @@ if (!allowedTo(str_replace(array(dirname(__FILE__), '\\', '/', '.php'), '', __FI
 function ShowMultiIPPage()
 {
 	global $LNG, $db;
-	$Query	= $db->query("SELECT id, username, user_lastip FROM ".USERS," WHERE (user_lastip) IN (SELECT user_lastip FROM uni1_users GROUP BY user_lastip HAVING COUNT(*)>1) ORDER BY user_lastip ASC;");
+	$Query	= $db->query("SELECT id, username, user_lastip FROM ".USERS." WHERE `universe` = '".$_SESSION['adminuni']."' AND user_lastip IN (SELECT user_lastip FROM uni1_users WHERE `universe` = '".$_SESSION['adminuni']."' GROUP BY user_lastip HAVING COUNT(*)>1) ORDER BY user_lastip ASC;");
 	$IPs	= array();
 	while($Data = $db->fetch_array($Query)) {
 		if(!isset($IPs[$Data['user_lastip']]))
