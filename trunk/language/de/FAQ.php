5a1c76240620df2fc5e2b6583c47f4eab8fd1835
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-01-21 12:22:52 +0000

    Updated JS libs Changed Copyright Header | Part 3

--- trunk/language/de/FAQ.php
+++ trunk/language/de/FAQ.php
@@ -1,5 +1,31 @@
 <?php
 
+/**
+ *  2Moons
+ *  Copyright (C) 2011  Slaver
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package 2Moons
+ * @author Slaver <slaver7@gmail.com>
+ * @copyright 2009 Lucky <douglas@crockford.com> (XGProyecto)
+ * @copyright 2011 Slaver <slaver7@gmail.com> (Fork/2Moons)
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 1.3 (2011-01-21)
+ * @link http://code.google.com/p/2moons/
+ */
+
 $LNG['faq']	= array(
 "Welche Rohstoffe gibt es ?" => "Die Hauptrohstoffe sind Metall, Kristall und Deuterium. Diese werden für Gebäudebau, Forschung, Schiffsbau undden Bau von Verteidigungsanlagen benötigt. Die Energie lässt deine Rohstoffwerke auf 100% arbeiten. Solltest du weniger Energie produzieren wie du benötigst, verringert sich deine Werksproduktion der Rohstoffe. <br>",
 "Was ist die kleine grüne Zahl unter den Rohstoffen ?  " => "Diese Zahl gibt deine Lagergröße an. Sind die Lager voll, produzieren deine Werke keine Rohstoffe mehr.<br>",
