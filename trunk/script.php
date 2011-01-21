59936942e84b5cf51427d47633f592acda1acc0b
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-01-21 21:31:33 +0000

    Fixed PHP Error on BCMath Wrapper Fixed JS Iusses

--- trunk/script.php
+++ trunk/script.php
@@ -26,36 +26,35 @@
  * @link http://code.google.com/p/2moons/
  */
 
-header('Cache-Control: must-revalidate, max-age=86400');
-header('Expires: '.gmdate('D, d M Y H:i:s', time() + 86400).' GMT');
+ini_set('zlib.output_compression', 'Off');
 header('Content-Type: application/x-javascript');
-
-ini_set('zlib.output_compression', 'On');
 define('ROOT_PATH', str_replace('\\', '/',dirname(__FILE__)).'/');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 if(!isset($_GET['script']))
 	exit(header('HTTP/1.1 204 No Content'));
-
-$CACHE	= ROOT_PATH.'cache/'.md5($_GET['script']).'.js.php';
-if(file_exists($CACHE) && !isset($_GET['cache'])) {
-	include($CACHE);
-	exit;
-} else {
-	file_put_contents($CACHE, '');
-}
-
-require_once(ROOT_PATH.'includes/libs/JSMin/jsmin.php');
 
 $SCRIPTS	= explode(';', str_replace(array('/', '\\', '-', '.'), '', $_GET['script']));
 if(!is_array($SCRIPTS))
 	exit(header('HTTP/1.1 204 No Content'));
 	
-foreach($SCRIPTS as $FILE) {
-	$JSSOUCRE	= JSMin::minify(file_get_contents(ROOT_PATH.'scripts/'.$FILE.'.js'));
-	echo $JSSOUCRE;
-	file_put_contents($CACHE, $JSSOUCRE, FILE_APPEND);
+$JSSOUCRE	= '';
+foreach($SCRIPTS as $FILE)
+	$JSSOUCRE	.= file_get_contents(ROOT_PATH.'scripts/'.$FILE.'.js')."\n";
+
+$ETag	= md5($JSSOUCRE);
+header('ETag: '.$ETag);
+if(isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+	if($_SERVER['HTTP_IF_NONE_MATCH'] == $ETag) {
+		header('HTTP/1.0 304 Not Modified');
+		exit;
+	} elseif(file_exists(ROOT_PATH.'cache/'.$_SERVER['HTTP_IF_NONE_MATCH'].'.js.php')) {
+		unlink(ROOT_PATH.'cache/'.$_SERVER['HTTP_IF_NONE_MATCH'].'.js.php');
+	}
 }
+
+require_once(ROOT_PATH.'includes/libs/JSMin/jsmin.php');
+$JSMIN	= JSMin::minify($JSSOUCRE);
+file_put_contents(ROOT_PATH.'cache/'.$ETag.'.js.php', $JSMIN);
+echo $JSMIN;
 
 ?>
