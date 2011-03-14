4187d3a2659d2db2ac43dfdf75f252e798a0ae73
Jan Kröpke <kroepke@outlook.com>
2011-03-14 23:19:26 +0000

    Fixed Installer

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,6 @@
 function submitftp()
 {
-	$.get('install.php?mode=ajax&action=ftp&lang='+location.search.split('&')[1].substr('-2')+'&'+$('#ftp').serialize(), function(data) {
+	$.get('?mode=ajax&action=ftp&lang='+location.search.split('&')[1].substr('-2')+'&'+$('#ftp').serialize(), function(data) {
 		if(data == "")
 			document.location.reload();
 		else
@@ -10,7 +10,7 @@ function submitftp()
 
 function submitinstall()
 {
-	$.getJSON('install.php?mode=ajax&action=install&lang='+location.search.split('&')[2].substr('-2')+'&'+$('#install').serialize(), function(data) {
+	$.getJSON('?mode=ajax&action=install&lang='+location.search.split('&')[2].substr('-2')+'&'+$('#install').serialize(), function(data) {
 		alert(data.msg);
 		if(!data.error)
 			document.location.href = 'install.php?mode=ins&page=2&lang='+location.search.split('&')[2].substr('-2');
