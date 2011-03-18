cc0733440bd439a1dcd758f387137fb0ad296f49
Jan Kröpke <kroepke@outlook.com>
2011-03-18 23:16:32 +0000

    Updated filelist Use new jQuery tooltop instead of old overLIB (Beta)

--- scripts/base.js
+++ scripts/base.js
@@ -226,3 +226,30 @@ var Dialog	= {
 }
 
 alert = Dialog.alert;
+
+function executeToolTip(){
+    $(".tooltip").hover(function(){
+        tip = $('#tooltip');
+        tip.html($(this).attr('name'));
+        tip.show(); //Show tooltip
+		return true;
+    }, function() {
+        tip.hide();
+		return false; //Hide tooltip
+    }).mousemove(function(e) {
+        var mousex = e.pageX + 20; //Get X coodrinates
+        var mousey = e.pageY + 20; //Get Y coordinates
+        var tipWidth = tip.width(); //Find width of tooltip
+        var tipHeight = tip.height(); //Find height of tooltip
+
+        var tipVisX = $(window).width() - (mousex + tipWidth);
+        var tipVisY = $(window).height() - (mousey + tipHeight);
+
+        if ( tipVisX < 20 ) { //If tooltip exceeds the X coordinate of viewport
+            mousex = e.pageX - tipWidth - 20;
+        } if ( tipVisY < 20 ) { //If tooltip exceeds the Y coordinate of viewport
+            mousey = e.pageY - tipHeight - 20;
+        }
+        tip.css({  top: mousey, left: mousex });
+    });
+}
