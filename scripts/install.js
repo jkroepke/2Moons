5eefef25348345318d2bd560001a1c204bc92f07
Jan Kröpke <kroepke@outlook.com>
2011-03-19 17:47:05 +0000

    Fixed Installer

--- scripts/install.js
+++ scripts/install.js
@@ -11,10 +11,9 @@ function submitftp()
 function submitinstall()
 {
 	$.getJSON('?mode=ajax&action=install&lang='+location.search.split('&')[2].substr('-2')+'&'+$('#install').serialize(), function(data) {
-		Dialog.alert(data.msg, function({
-			if(!data.error)
-				document.location.href = '?mode=ins&page=2&lang='+location.search.split('&')[2].substr('-2');
-		}));
+		alert(data.msg);
+		if(!data.error)
+			document.location.href = '?mode=ins&page=2&lang='+location.search.split('&')[2].substr('-2');
 	});
 	return false;
 }
