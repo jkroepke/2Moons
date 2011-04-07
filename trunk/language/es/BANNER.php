070e5d121086dea433958268a4ec6c8ffa45316f
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-04-07 21:20:31 +0000

    Updated Englsih Removed Spanish

--- trunk/language/es/BANNER.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$LNG['ub_points']							= 'Puntos';
-$LNG['ub_fleets']							= 'Flota';
-$LNG['ub_researh']							= 'Investigación';
-$LNG['ub_buildings']						= 'Edificios';
-$LNG['ub_defenses']							= 'Defensa';
-
-?>
