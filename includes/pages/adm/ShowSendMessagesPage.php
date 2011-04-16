68b8880f1b774434634d6345c349f3b765fd629d
Jan Kröpke <kroepke@outlook.com>
2011-04-16 21:06:03 +0000

    Added Mail Newsletter (sry cant test this)

--- includes/pages/adm/ShowSendMessagesPage.php
+++ includes/pages/adm/ShowSendMessagesPage.php
@@ -29,9 +29,9 @@
 if (!allowedTo(str_replace(array(dirname(__FILE__), '\\', '/', '.php'), '', __FILE__))) exit;
 
 function ShowSendMessagesPage() {
-	global $USER, $LNG, $db;
+	global $USER, $LNG, $db, $CONF;
 
-	if ($_GET['mode'] == 'send')
+	if ($_GET['action'] == 'send')
 	{
 		switch($USER['authlevel'])
 		{
@@ -48,17 +48,50 @@ function ShowSendMessagesPage() {
 		
 		$Subject	= request_var('subject', '', true);
 		$Message 	= makebr(request_var('text', '', true));
+		$Mode	 	= request_var('mode', 0);
 
 		if (!empty($Message) && !empty($Subject))
 		{
-			require_once(ROOT_PATH.'includes/functions/BBCode.php');
-			$Time    	= TIMESTAMP;
-			$From    	= '<span style="color:'.$color.';">'.$LNG['user_level'][$USER['authlevel']].' '.$USER['username'].'</span>';
-			$Subject 	= '<span style="color:'.$color.';">'.$Subject.'</span>';
-			$Message 	= '<span style="color:'.$color.';font-weight:bold;">'.bbcode($Message).'</span>';
+			if($Mode == 0 || $Mode == 2) {
+				require_once(ROOT_PATH.'includes/functions/BBCode.php');
+				$Time    	= TIMESTAMP;
+				$From    	= '<span style="color:'.$color.';">'.$LNG['user_level'][$USER['authlevel']].' '.$USER['username'].'</span>';
+				$Subject 	= '<span style="color:'.$color.';">'.$Subject.'</span>';
+				$Message 	= '<span style="color:'.$color.';font-weight:bold;">'.bbcode($Message).'</span>';
 
-			SendSimpleMessage(0, $USER['id'], TIMESTAMP, 50, $From, $Subject, $Message, 0, $_SESSION['adminuni']);
-			$db->query("UPDATE ".USERS." SET `new_gmessage` = `new_gmessage` + '1', `new_message` = `new_message` + '1' WHERE `universe` = '".$_SESSION['adminuni']."';");
+				SendSimpleMessage(0, $USER['id'], TIMESTAMP, 50, $From, $Subject, $Message, 0, $_SESSION['adminuni']);
+				$db->query("UPDATE ".USERS." SET `new_gmessage` = `new_gmessage` + '1', `new_message` = `new_message` + '1' WHERE `universe` = '".$_SESSION['adminuni']."';");
+			}
+			if($Mode == 1 || $Mode == 2) {
+				require_once(ROOT_PATH.'includes/classes/class.phpmailer.php');
+				$mail             	= new PHPMailer(true);
+				$mail->IsHTML(true);
+				if($CONF['mail_use'] == 2) {
+					$mail->IsSMTP();  
+					$mail->SMTPAuth   	= true; 
+					$mail->SMTPSecure 	= $CONF['smtp_ssl'];  						
+					$mail->Host      	= $CONF['smtp_host'];
+					$mail->Port      	= $CONF['smtp_port'];
+					$mail->Username  	= $CONF['smtp_user'];
+					$mail->Password  	= $CONF['smtp_pass'];
+					$mail->SMTPDebug  	= ($CONF['debug'] == 1) ? 2 : 0;   
+				} elseif($CONF['mail_use'] == 1) {
+					$mail->IsSendmail();
+					$mai->Sendmail		= $CONF['smail_path'];
+				} else {
+					$mail->IsMail();
+				}
+				$mail->CharSet		= 'UTF-8';		
+				$mail->Subject   	= $Subject;
+				$mail->Body   		= bbcode($Message);
+				$mail->SetFrom($CONF['smtp_sendmail'], $CONF['game_name']);
+				$mail->AddAddress($CONF['smtp_sendmail'], $CONF['game_name']);
+				$USERS	= $db->query("SELECT `username`, `email` FROM ".USERS." WHERE `universe` = '".$_SESSION['adminuni']."';");
+				while($UserData = $db->fetch_array($USERS)) {
+					$mail->AddBCC($UserData['email'], $UserData['username']);
+				}
+				$mail->Send();				
+			}
 			exit($LNG['ma_message_sended']);
 		} else {
 			exit($LNG['ma_subject_needed']);
@@ -74,6 +107,7 @@ function ShowSendMessagesPage() {
 		'ma_message' 				=> $LNG['ma_message'],
 		'ma_send_global_message' 	=> $LNG['ma_send_global_message'],
 		'ma_characters' 			=> $LNG['ma_characters'],
+		'ma_modes'		 			=> $LNG['ma_modes'],
 		'button_submit' 			=> $LNG['button_submit'],
 	));
 	
