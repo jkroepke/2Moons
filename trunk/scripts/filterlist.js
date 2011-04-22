50a5059a0ddf3066908a8d5fefc542787fe8e71a
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-04-22 18:52:43 +0000

    Changed SVN Properties Changed Copyright Header

--- trunk/scripts/filterlist.js
+++ trunk/scripts/filterlist.js
@@ -1,5 +1,5 @@
 /*==================================================*
- $Id: filterlist.js,v 1.3 2003/10/08 17:13:49 pat Exp $
+ $Id$
  Copyright 2003 Patrick Fitzgerald
  http://www.barelyfitz.com/webdesign/articles/filterlist/
 
