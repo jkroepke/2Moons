e9e52df0af9168c26b2f8affd453f6a385264cb0
Jan Kröpke <kroepke@outlook.com>
2011-06-15 19:33:02 +0000

    New FleetAJAX System

--- scripts/galaxy.js
+++ scripts/galaxy.js
@@ -1,59 +1,18 @@
-var strInfo = "";
-
 function doit (order, galaxy, system, planet, planettype, shipcount) {
-	$.post("game.php?page=fleetajax&ajax=1", {mission: order, galaxy: galaxy, system: system, planet: planet, planettype:  planettype, ships: shipcount}, function(data){
-		retVals   	= data.split("|");
-		Message   	= retVals[0];
-		Infos     	= retVals[1];
-		retVals   	= Infos.split(" ");
-		UsedSlots 	= retVals[0];
-		SpyProbes 	= retVals[1];
-		Recyclers 	= retVals[2];
-		GRecyclers	= retVals[3];
-		Missiles  	= retVals[4];
-		retVals  	= Message.split(";");
-		CmdCode  	= retVals[0];
-		strInfo  	= retVals[1];
-		if(CmdCode == 600)
-			addToTable(status_ok, "success");
-		else
-			addToTable(status_fail, "error");
-		
-		changeSlots(UsedSlots);
-		setShips("probes", SpyProbes );
-		setShips("recyclers", Recyclers );
-		setShips("grecyclers", GRecyclers );
-		setShips("missiles", Missiles );
-	});
-}
+	$.getJSON("game.php?page=fleetajax&ajax=1&mission="+order+"&galaxy="+galaxy+"&system="+system+"&planet="+planet+"&planettype="+planettype+"&ships="+shipcount, function(data){
+		$('#slots').text(data.slots);
+		$('#probes').text(number_format(data.ship210));
+		$('#recyclers').text(number_format(data.ship209));
+		$('#grecyclers').text(number_format(data.ship219));
 
-function addToTable(strDataResult, strClass) {
-	var e = document.getElementById('fleetstatusrow');
-	var e2 = document.getElementById('fleetstatustable');
-	e.style.display = '';
-	if(e2.rows.length > MaxFleetSetting) {
-		e2.deleteRow(MaxFleetSetting);
-	}
-	var row = e2.insertRow(0);
-	var td1 = document.createElement("td");
-	var td1text = document.createTextNode(strInfo);
-	td1.appendChild(td1text);
-	var td2 = document.createElement("td");
-	var span = document.createElement("span");
-	var spantext = document.createTextNode(strDataResult);
-	var spanclass = document.createAttribute("class");
-	spanclass.nodeValue = strClass;
-	span.setAttributeNode(spanclass);
-	span.appendChild(spantext);
-	td2.appendChild(span);
-	row.appendChild(td1);
-	row.appendChild(td2);
-}
-function changeSlots(add) {
-	$('#slots').text(add);
-}
-function setShips(ship, count) {
-	$('#'+ship).text(number_format(count));
+		var statustable	= $('#fleetstatusrow');
+		var messages	= statustable.children();
+		if(messages.length == MaxFleetSetting) {
+			messages.get(MaxFleetSetting - 1).remove();
+		}
+		statustable.removeAttr('style').prepend($('<td />').attr('colspan', 8).attr('class', data.code == 600 ? "success" : "error").text(data.mess));
+		
+	});
 }
 
 function galaxy_submit(value) {
