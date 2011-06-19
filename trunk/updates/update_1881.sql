2120185b64cd6d72528a2f5c8ccb0e33fb1f9d1e
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-06-19 00:02:18 +0000

    Fixed SQL Syntax Error .-

--- trunk/updates/update_1881.sql
+++ trunk/updates/update_1881.sql
@@ -7,7 +7,7 @@ ADD `new_message_4` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_messag
 ADD `new_message_5` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_4`, 
 ADD `new_message_15` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_5`, 
 ADD `new_message_50` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_15`, 
-ADD `new_message_99` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_50
+ADD `new_message_99` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_50;
 ALTER TABLE `prefix_users`
 DROP `new_message`,
 DROP `new_gmessage`;
@@ -21,4 +21,4 @@ DROP INDEX `authlevel`,
 DROP INDEX `dm_fleettime`, 
 DROP INDEX `register_time`, 
 DROP INDEX `universe`, 
-ADD INDEX `universe` ( `universe` , `username` , `password` , `onlinetime` , `authlevel` )
+ADD INDEX `universe` ( `universe` , `username` , `password` , `onlinetime` , `authlevel` );
