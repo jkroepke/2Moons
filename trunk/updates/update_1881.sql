4883740fbe5ea9cc566060f6955718d6cd423475
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2011-06-19 10:16:35 +0000

    Fixed Syntax Error again. Sry it was to late :$

--- trunk/updates/update_1881.sql
+++ trunk/updates/update_1881.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `uni1_users` 
+ALTER TABLE `prefix_users` 
 ADD `new_message_0` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `onlinetime`, 
 ADD `new_message_1` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_0`, 
 ADD `new_message_2` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_1`, 
@@ -7,14 +7,14 @@ ADD `new_message_4` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_messag
 ADD `new_message_5` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_4`, 
 ADD `new_message_15` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_5`, 
 ADD `new_message_50` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_15`, 
-ADD `new_message_99` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_50;
-ALTER TABLE `prefix_users`
-DROP `new_message`,
+ADD `new_message_99` TINYINT( 3 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `new_message_50`;
+ALTER TABLE `prefix_users` 
+DROP `new_message`, 
 DROP `new_gmessage`;
-ALTER TABLE `prefix_users`
-CHANGE `kbmetal` `kbmetal` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0',
-CHANGE `kbcrystal` `kbcrystal` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0',
-CHANGE `lostunits` `lostunits` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0',
+ALTER TABLE `prefix_users` 
+CHANGE `kbmetal` `kbmetal` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0', 
+CHANGE `kbcrystal` `kbcrystal` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0', 
+CHANGE `lostunits` `lostunits` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0', 
 CHANGE `desunits` `desunits` DECIMAL( 65 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `prefix_users` 
 DROP INDEX `authlevel`, 
