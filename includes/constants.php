12aa9814dcf904d6a771aa3a3fbbcab510e865e8
Jan Kröpke <kroepke@outlook.com>
2011-10-05 12:41:59 +0000

    remove test vars

--- includes/constants.php
+++ includes/constants.php
@@ -28,7 +28,7 @@
  */
 
 //SET TIMEZONE (if Server Timezone are not correct)
-date_default_timezone_set('America/Chicago');
+//date_default_timezone_set('Europe/Berlin');
  
 //TEMPLATES DEFAULT SETTINGS
 define('DEFAULT_THEME'	 		  , 'gow');
