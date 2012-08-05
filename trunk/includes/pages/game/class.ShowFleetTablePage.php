09839c8682701f2e1098552db2f2e7b90feeb6ce
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2012-08-05 16:46:49 +0000

    Fix mission shortcuts from galaxy (BUg #7;Orion)

--- trunk/includes/pages/game/class.ShowFleetTablePage.php
+++ trunk/includes/pages/game/class.ShowFleetTablePage.php
@@ -196,7 +196,7 @@ class ShowFleetTablePage extends AbstractPage
 		$targetGalaxy	= HTTP::_GP('galaxy', $PLANET['galaxy']);
 		$targetSystem	= HTTP::_GP('system', $PLANET['system']);
 		$targetPlanet	= HTTP::_GP('planet', $PLANET['planet']);
-		$targetType		= HTTP::_GP('type', $PLANET['planet_type']);
+		$targetType	= HTTP::_GP('planettype', $PLANET['planet_type']);
 		$targetMission	= HTTP::_GP('mission', 0);
 		
 		$fleetResult 		= $GLOBALS['DATABASE']->query("SELECT * FROM ".FLEETS." WHERE fleet_owner = ".$USER['id']." AND fleet_mission <> 10 ORDER BY fleet_end_time ASC;");
