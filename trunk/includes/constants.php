aea7b2541976e5b9676bf0ecf69235c7a0311b55
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2012-11-11 17:51:44 +0000

    Fix redirect error, of url contains special chars.

--- trunk/includes/constants.php
+++ trunk/includes/constants.php
@@ -33,8 +33,8 @@
 define('DEFAULT_THEME'	 		    , 'gow');
 define('HTTPS'						, isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"]  == 'on');
 define('PROTOCOL'					, HTTPS ? 'https://' : 'http://');
-define('HTTP_BASE'					, str_replace('\\', '/', str_replace(basename($_SERVER['SCRIPT_FILENAME']), '', parse_url($_SERVER['SCRIPT_NAME'], PHP_URL_PATH))));
-define('HTTP_ROOT'					, str_replace('\\', '/', str_replace(basename($_SERVER['SCRIPT_FILENAME']), '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))));
+define('HTTP_BASE'					, str_replace('\\', '/', str_replace(basename($_SERVER['SCRIPT_FILENAME']), '', urldecode(parse_url($_SERVER['SCRIPT_NAME'], PHP_URL_PATH)))));
+define('HTTP_ROOT'					, str_replace('\\', '/', str_replace(basename($_SERVER['SCRIPT_FILENAME']), '', urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)))));
 define('HTTP_FILE'					, str_replace(HTTP_ROOT, '', $_SERVER['REQUEST_URI']));
 define('HTTP_HOST'					, $_SERVER['HTTP_HOST']);
 define('HTTP_PATH'					, PROTOCOL.HTTP_HOST.HTTP_ROOT);
