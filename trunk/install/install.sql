ac3c2f6fa9ed0945235af33036a8e83ff0a70062
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2013-01-05 14:11:03 +0000

    After a successfully deploy mission give the half consumption back. (FIX #298)

--- trunk/install/install.sql
+++ trunk/install/install.sql
@@ -353,6 +353,7 @@ CREATE TABLE `%PREFIX%fleets` (
   `fleet_mess` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `start_time` int(11) DEFAULT NULL,
   `fleet_busy` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `hasCanceled` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`fleet_id`),
   KEY `fleet_target_owner` (`fleet_target_owner`,`fleet_mission`),
   KEY `fleet_owner` (`fleet_owner`,`fleet_mission`),
--- trunk/scripts/game/flotten.js
+++ trunk/scripts/game/flotten.js
@@ -7,7 +7,6 @@ function updateVars()
 	dataFlyTime = GetDuration();
 	dataFlyConsumption = GetConsumption();
 	dataFlyCargoSpace = storage();
-	dataFlyTime = dataFlyTime * data.fleetspeedfactor
 	refreshFormData();
 }
 
@@ -32,7 +31,7 @@ function GetDistance() {
 
 function GetDuration() {
 	var sp = document.getElementsByName("speed")[0].value;
-	return Math.max(Math.round((3500 / (sp * 0.1) * Math.pow(dataFlyDistance * 10 / data.maxspeed, 0.5) + 10) / data.gamespeed), 5);
+	return Math.max(Math.round((3500 / (sp * 0.1) * Math.pow(dataFlyDistance * 10 / data.maxspeed, 0.5) + 10) / data.gamespeed) * data.fleetspeedfactor, 5);
 }
 
 function GetConsumption() {
