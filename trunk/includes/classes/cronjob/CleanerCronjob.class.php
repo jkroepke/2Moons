2e74e999b95ce6575ea45a51619d34dae0792b54
slaver7 <slaver7@b16e238e-0886-11df-aae1-e754eacfc418>
2013-01-05 13:42:03 +0000

    Clean invalid entires on fleet_events table

--- trunk/includes/classes/cronjob/CleanerCronjob.class.php
+++ trunk/includes/classes/cronjob/CleanerCronjob.class.php
@@ -36,7 +36,7 @@ class CleanerCronjob
 		$unis	= array_keys(Config::getAll(NULL));
 		
 		$GLOBALS['DATABASE']->query("LOCK TABLES ".ALLIANCE." WRITE, ".ALLIANCE_REQUEST." WRITE,
-									".BUDDY." WRITE, ".CONFIG." WRITE, ".FLEETS." WRITE, 
+									".BUDDY." WRITE, ".CONFIG." WRITE, ".FLEETS." WRITE, , ".FLEETS_EVENT." WRITE, 
 									".NOTES." WRITE, ".MESSAGES." WRITE, ".PLANETS." WRITE, 
 									".RW." WRITE, ".SESSION." WRITE, ".STATPOINTS." WRITE, 
 									".TOPKB." WRITE, ".TOPKB_USERS." WRITE, ".USERS." WRITE;");
@@ -50,6 +50,7 @@ class CleanerCronjob
 						  DELETE FROM ".ALLIANCE." WHERE `ally_members` = '0';
 						  DELETE FROM ".PLANETS." WHERE `destruyed` < ".TIMESTAMP." AND `destruyed` != 0;
 						  DELETE FROM ".SESSION." WHERE `lastonline` < '".(TIMESTAMP - SESSION_LIFETIME)."';
+						  DELETE FROM ".FLEETS_EVENT." WHERE fleetID NOT IN (SELECT fleet_id FORM ".FLEETS.");
 						  UPDATE ".USERS." SET `email_2` = `email` WHERE `setmail` < '".TIMESTAMP."';");
 
 		$ChooseToDelete = $GLOBALS['DATABASE']->query("SELECT `id` FROM `".USERS."` WHERE `authlevel` = '".AUTH_USR."' AND ((`db_deaktjava` != 0 AND `db_deaktjava` < '".$del_deleted."')".($del_inactive == TIMESTAMP ? "" : " OR `onlinetime` < '".$del_inactive."'").");");
