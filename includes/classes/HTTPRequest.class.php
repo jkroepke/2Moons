f0bf1e102cec4f427da160471b07e3c92b5e49d2
Jan Kröpke <kroepke@outlook.com>
2013-02-04 14:48:53 +0000

    Remove CURLOPT_FOLLOWLOCATION due safemore (FIX #364)

--- includes/classes/HTTPRequest.class.php
+++ includes/classes/HTTPRequest.class.php
@@ -47,7 +47,6 @@ class HTTPRequest
 			$this->ch	= curl_init($this->url);
 			curl_setopt($this->ch, CURLOPT_HTTPGET, true);
 			curl_setopt($this->ch, CURLOPT_AUTOREFERER, true);
-			curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($this->ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; 2Moons/".Config::get('VERSION')."; +http://2moons.cc)");
 			curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(
