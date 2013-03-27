2c5541850df1ba5a6341695f32d24a2d245aca0a
lordmegger@googlemail.com <lordmegger@googlemail.com@b16e238e-0886-11df-aae1-e754eacfc418>
2013-03-27 15:08:00 +0000

    Custom lang keys dont work (FIX #429)

--- trunk/includes/classes/Language.class.php
+++ trunk/includes/classes/Language.class.php
@@ -2,7 +2,7 @@
 
 /**
  *  2Moons
- *  Copyright (C) 2012 Jan Kröpke
+ *  Copyright (C) 2012 Jan Krï¿½pke
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,8 +18,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package 2Moons
- * @author Jan Kröpke <info@2moons.cc>
- * @copyright 2012 Jan Kröpke <info@2moons.cc>
+ * @author Jan Krï¿½pke <info@2moons.cc>
+ * @copyright 2012 Jan Krï¿½pke <info@2moons.cc>
  * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
  * @version 1.7.2 (2013-03-18)
  * @info $Id$
@@ -119,7 +119,7 @@ class Language implements ArrayAccess {
     }
 	
     public function addData($data) {
-		$this->container = array_merge_recursive($this->container, $data);
+		$this->container = array_replace_recursive($this->container, $data);
     }
 	
 	public function getLanguage()
