312ef78e4c7ec9793e993aec3525a028eddbf606
Jan <kroepke@outlook.com>
2015-10-16 21:48:11 +0200

    Fixed some stupid titles on building

--- scripts/game/buildlist.js
+++ scripts/game/buildlist.js
@@ -14,28 +14,29 @@ function Buildlist() {
 		window.setTimeout(function() {
 			window.location.href = 'game.php?page=buildings';
 		}, 1000);
-		return true;
+		return;
 	}
-	document.title	= GetRestTimeFormat(rest) + ' - ' + buildname + ' - ' + Gamename;
+	document.title = GetRestTimeFormat(rest) + ' - ' + buildname + ' - ' + Gamename;
 	
 	$('#time').text(GetRestTimeFormat(rest));
-}
-
-function CreateProcessbar() {
-	if(time != 0) {
-		$('#progressbar').progressbar({
-			value: Math.max(100 - (resttime / time) * 100, 0.01)
-		});
-		$('.ui-progressbar-value').addClass('ui-corner-right').animate({width: "100%"}, resttime * 1000, "linear");
-	}
 }
 
 $(document).ready(function() {
 	time		= $('#time').data('time');
 	resttime	= $('#progressbar').data('time');
 	endtime		= $('.timer:first').data('time');
-	buildname	= $('.onlist:first').text();
-	interval	= window.setInterval(Buildlist, 1000);
-	window.setTimeout(CreateProcessbar, 5);
+	buildname	= $('.buildlist > table > tbody > tr > td:first').text().replace(/[0-9]+\.:/, '').trim();
+    interval	= window.setInterval(Buildlist, 1000);
+
+	window.setTimeout(function () {
+        if(time <= 0) return;
+
+        $('#progressbar').progressbar({
+            value: Math.max(100 - (resttime / time) * 100, 0.01)
+        });
+        $('.ui-progressbar-value').addClass('ui-corner-right').animate({width: "100%"}, resttime * 1000, "linear");
+    }, 5);
+
+
 	Buildlist();
 });
