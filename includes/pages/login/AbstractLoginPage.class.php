b539e834f7a19557234c2152b1ff4598f5ac8c1b
Ozan Kurt <me@ozankurt.com>
2016-04-25 21:15:46 +0300

    Fixed basePath calculation if the universe is already in the subdomain

--- includes/pages/login/AbstractLoginPage.class.php
+++ includes/pages/login/AbstractLoginPage.class.php
@@ -142,11 +142,20 @@ abstract class AbstractLoginPage
 		if($this->getWindow() !== 'ajax') {
 			$this->getPageData();
 		}
+
+		if (UNIS_WILDCAST) {
+			$hostParts = explode('.', HTTP_HOST);
+			array_shift($hostParts);
+			$host = implode('.', $hostParts);
+			$basePath = PROTOCOL.$host.HTTP_BASE;
+		} else {
+			$basePath = PROTOCOL.HTTP_HOST.HTTP_BASE;
+		}
 		
 		$this->assign(array(
             'lang'    			=> $LNG->getLanguage(),
 			'bodyclass'			=> $this->getWindow(),
-			'basepath'			=> PROTOCOL.HTTP_HOST.HTTP_BASE,
+			'basepath'			=> $basePath,
 			'isMultiUniverse'	=> count(Universe::availableUniverses()) > 1,
 			'unisWildcast'		=> UNIS_WILDCAST,
 		));
