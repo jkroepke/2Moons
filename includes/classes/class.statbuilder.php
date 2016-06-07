c9b690dcb7b55b4fe3342a5d9d628f0250f51d4d
Jan-Otto Kröpke <jan-otto.kroepke@noris.de>
2016-06-07 19:52:06 +0200

    Fixed #211 SQL error on statbuilder, if game has x * 50 - 1 users.

--- includes/classes/class.statbuilder.php
+++ includes/classes/class.statbuilder.php
@@ -395,7 +395,7 @@ class statbuilder
 			}
 		}
 
-		if (!empty($FinalSQL)) {
+		if (!empty($FinalSQL) && $FinalSQL != $tableHeader) {
 		    $FinalSQL = substr($FinalSQL, 0, -2).';';
             $this->SaveDataIntoDB($FinalSQL);
             unset($UserPoints);
