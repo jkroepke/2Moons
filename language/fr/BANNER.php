c11882542c648e5b631a1e807e30124182ab684f
BigTwoProduction <contact.bigtwoprod@gmail.com>
2016-08-23 23:08:22 +0200

    French Translation  (#253)
    * traduction FR
    
    * Tranlastion FR
    
    * Tranlastion FR with UTF-8 format

---
+++ language/fr/BANNER.php
@@ -0,0 +1,15 @@
+<?php
+// Traduction Française by BigTwoProduction (KickXAss4Ever & Apocalypto2202) - All rights reserved (C) 2016
+// Web : http://www.big-two.tk
+// Version 1.0
+
+$LNG['ub_points']							= 'Points';
+$LNG['ub_fleets']							= 'Flotte';
+$LNG['ub_researh']							= 'Recherche';
+$LNG['ub_buildings']						= 'Bâtiments';
+$LNG['ub_defenses']							= 'D&eacute;fense';
+
+$LNG['ub_fights']		= 'Batailles';
+$LNG['ub_quote']		= 'Citer';
+$LNG['ub_rank']			= 'Position';
+?>
---
+++ language/fr/CHANGELOG.php
@@ -0,0 +1,13 @@
+<?php
+// Traduction Française by BigTwoProduction (KickXAss4Ever & Apocalypto2202) - All rights reserved (C) 2016
+// Web : http://www.big-two.tk
+
+$LNG['Version']     = 'Version';
+$LNG['Description'] = 'Description';
+
+$LNG['changelog']   = array(
+
+
+
+);
+?>
