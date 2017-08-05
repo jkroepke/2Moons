f87c8d5bad54b51e416b943a8c06fbd2205f6684
Jan-Otto Kröpke <kroepke@outlook.com>
2017-08-05 13:38:08 +0200

    FIXED ENV

---
+++ tests/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('chrome');
+        $this->setBrowserUrl('http://localhost:8888/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('http://localhost:8888/');
+        $this->assertEquals('2Moons');
+    }
+
+}
--- tests/run.js
+++
@@ -1,9 +0,0 @@
-console.log('Start running 2moons tests');
-
-var page = require('webpage').create();
-var prefix = 'http://localhost:8000';
-
-
-page.open(prefix, function(status) {
-    console.log(status);
-});
