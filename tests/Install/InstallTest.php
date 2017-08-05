bb0edfd9a4690e9067392d5b3ebafd2efccd5cfe
Jan-Otto Kröpke <kroepke@outlook.com>
2017-08-05 17:55:35 +0200

    Test w/ php http client

--- tests/Install/InstallTest.php
+++ tests/Install/InstallTest.php
@@ -1,12 +1,14 @@
 <?php
 
-class InstallTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class InstallTest extends TestCase
 {
     private $http;
 
     protected function setUp()
     {
-        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://localhost:8888/']);
+        $this->http = new GuzzleHttp\Client(['base_uri' => $_ENV['TEST_BASE_URI']]);
     }
 
     public function tearDown() {
@@ -15,8 +17,28 @@ class InstallTest extends PHPUnit_Framework_TestCase
 
     public function testInstallRedirect()
     {
-        $response = $this->http->request('GET', '');
+        $response = $this->http->request('GET', '/', ['allow_redirects' => false]);
 
         $this->assertEquals(302, $response->getStatusCode());
+
+        $location = $response->getHeaders()["Location"][0];
+        $this->assertEquals('install/index.php', $location);
+    }
+
+    public function testInstallHome()
+    {
+        $response = $this->http->request('GET', '/install/index.php');
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testInstallStep2()
+    {
+        $response = $this->http->request('GET', '/index.php?mode=install&step=2');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $this->assertRegexp('/<span class="yes">/', $response->getBody());
+        $this->assertRegexp('/<a href="index.php?mode=install&step=3">/', $response->getBody());
     }
 }
