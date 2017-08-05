e8b87b757a2a6e8a62d67280b364e3bbb6d3933d
Jan-Otto Kröpke <kroepke@outlook.com>
2017-08-05 15:13:22 +0200

    FIXED ENV

--- tests/Install/InstallTest.php
+++ tests/Install/InstallTest.php
@@ -4,13 +4,15 @@ class InstallTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected function setUp()
     {
+        $this->setHost('localhost');
+        $this->setPort(4444);
         $this->setBrowser('chrome');
         $this->setBrowserUrl('http://localhost:8888/');
     }
 
     public function testTitle()
     {
-        $this->url('http://localhost:8888/');
+        $this->url('/');
         $this->assertEquals('2Moons');
     }
 }
